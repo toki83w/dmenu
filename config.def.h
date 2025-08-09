8623315497adb3170ee082be5cceb3f2a6e0c16d
toki83 <toki83w@gmail.com>
2025-08-09 14:30:06 +0200

    [PATCH] fuzzymatch: Add support for fuzzy-matching

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int min_width = 500;                   /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
