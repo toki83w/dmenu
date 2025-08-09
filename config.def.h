8314006da38ac271bc51fff7d7b033132f04d1cd
toki83 <toki83w@gmail.com>
2025-08-09 17:37:33 +0200

    [CONFIG] Set Noto Sans font

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static int min_width = 500;                   /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"NotoSansM NFP:size=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
